d39e565b0b21f3d3a76ee932bb0181607eaadfb8
Warinya Rojanasuwan <wariroja@gmail.com>
2022-08-17 11:29:40 -0700

    chore: include file changes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 
 const express = require('express');
 const Datastore = require('nedb')
+const path = require('path')
 const app = express();
 const cors = require('cors');
 const { response } = require('express');
 const database = new Datastore('database.db');
 database.loadDatabase();
-
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors())
 app.use(express.json())
 
 app.get("/api", (req, res) => {
+    console.log({req})
     res.set('Access-Control-Allow-Origin', '*');
     database.find({}, (err, data) => {
         if(err) {
@@ -37,3 +39,5 @@ app.post("/api", (req, res) => {
 app.listen(9000, () => {
     console.log('listening on http://localhost:9000')
 })
+
+module.exports = app;
